09f9651e757b676e7285b3ef626da6d24a374e2c
Purvinis <g.purvinis@gmail.com>
2020-08-16 06:23:02 -1000

    quiz 3 questions and final answers. Many installs to do this quiz

--- Course3Week1Quiz1.R
+++ Course3Week1Quiz1.R
@@ -2,7 +2,8 @@
 #The dataset is about housing
 #How many properties are worth $1,000,000 or more?  (VAL)
 #Consider the variable FES in the code book. Which of the "tidy data" principles
-#does this variable violate? 
+#does this variable violate? represent marital status and employment together in one variable
+#should be split
 
 setwd("D:/Rwork/datasciencecoursera")
 library(dplyr)  #already installed, just need to load
@@ -24,7 +25,8 @@ housingData <-read.table("./data/q1data.csv",sep = ',',header = TRUE)
 print(head(housingData))
 
 housingValues <- select(housingData,VAL) %>% filter(VAL ==24)
-print(dim(housingValues))
+ans1 <-nrow(housingValues)
+print(paste(ans1,"number of houses over 1M"))
 
 print(select(housingData,FES))
 
@@ -44,6 +46,7 @@ q3url <-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xl
 download.file(q3url, destfile = "./data/q3data.xlsx",mode='wb')
 dat <-read.xlsx("./data/q3data.xlsx",sheetIndex = 1 , rowIndex = 18:23,colIndex = 7:15)
 ans3 <-sum(dat$Zip*dat$Ext,na.rm=T)
+print(paste(ans3,"sum"))
 #---------------------------------------------------------------
 #Q4 Read the XML data on Baltimore restaurants from here:
 #https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml
@@ -62,7 +65,19 @@ print(xmlSize(rootNode[[1]][[1]]))
 listOfZips <-xpathSApply(rootNode,"//zipcode",xmlValue)
 ans4 <- distinct(data.frame(listOfZips)) #number of distinct zipcodes (oops) =32
 finalans4 <- sum(listOfZips == "21231")
+print(paste(finalans4," restaurants in zip"))
 
+#alternate
+#sum(xpathSApply(rootNode,"//zipcode",xmlValue)=="21231)
 #--------------------------------------------------------------
 
+
 q5url <-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(q5url, destfile = "./data/q5data.csv")
+DT <-fread( file = ,"./data/q5data.csv")
+system.time(DT[,mean(pwgtp15,by=SEX)]) #uses dt features because you don't have to use $
+#system.time(DT[DT$SEX==1,]$pwgtp15);mean(DT[DT$SEX==2,]$pwgtp15)#2 parts, slow
+#system.time(tapply(DT$pwgtp15,DT$SEX,mean))# uses tappy
+#system.time(rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2])#bad code
+#system.time(sapply(split(DT$pwgtp15,DT$SEX),mean))#uses sapply and is fastest
+system.time(mean(DT$pwgtp15,by=DT$SEX))
